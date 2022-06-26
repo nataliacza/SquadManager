258e061501ed839e371781e06e8fd819834dbb9a
Natalia Czapska <czapskanatalia@gmail.com>
2022-06-26 18:46:06 +0200

    Updated FluidValidation. Issue with duplicated messages to be resolved

--- SquadManager.Dtos/Members/SaveMemberDto.cs
+++ SquadManager.Dtos/Members/SaveMemberDto.cs
@@ -1,24 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace SquadManager.Dtos.Members;
+﻿namespace SquadManager.Dtos.Members;
 
 public class SaveMemberDto
 {
-    [Required]
-    [MaxLength(30)]
     public string FirstName { get; set; } = null!;
-
-    [Required]
-    [MaxLength(30)]
     public string LastName { get; set; } = null!;
-
-    [Required]
-    [EmailAddress]
-    [MaxLength(50)]
     public string Email { get; set; } = null!;
-    
-    [Required]
-    [Phone]
-    [MinLength(9)][MaxLength(10)]
     public string Mobile { get; set; } = null!;
 }
---
+++ SquadManager.Services/Validation/EfMemberCreatorValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using SquadManager.Dtos.Members;
+
+namespace SquadManager.Services.Validation;
+
+public class EfMemberCreatorValidator : AbstractValidator<SaveMemberDto>
+{
+    public EfMemberCreatorValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .MaximumLength(30)
+            .NotEmpty();
+
+        RuleFor(x => x.LastName)
+            .MaximumLength(30)
+            .NotEmpty();
+
+        RuleFor(x => x.Email)
+            .EmailAddress()
+            .MaximumLength(50)
+            .NotEmpty();
+
+        RuleFor(x => x.Mobile)
+            .MinimumLength(9)
+            .MaximumLength(10)
+            .Matches(@"\d")
+            .NotEmpty();
+    }
+}
