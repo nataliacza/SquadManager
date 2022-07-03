8c30e2af070cb013803264a87a39cd101b91a442
Natalia Czapska <czapskanatalia@gmail.com>
2022-07-03 16:46:45 +0200

    Add validator and update dto

---
+++ SquadManager.Services/Validation/Dog/CreateDogValitator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using SquadManager.Dtos.Dogs;
+
+namespace SquadManager.Services.Validation.Member;
+
+public class CreateDogValidator : AbstractValidator<CreateDogDto>
+{
+    public CreateDogValidator()
+    {
+        RuleFor(x => x.Name)
+            .MaximumLength(30)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(x => x.OwnerId)
+            .NotNull()
+            .NotEmpty();
+    }
+}
