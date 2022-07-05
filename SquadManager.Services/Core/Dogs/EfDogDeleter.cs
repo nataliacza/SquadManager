a947c8b34dbc4cacc5a5e05d3c28a9792078b1d9
Natalia Czapska <czapskanatalia@gmail.com>
2022-07-05 13:10:01 +0200

    Dog deleter inferface created and registered

---
+++ SquadManager.Services/Core/Dogs/EfDogDeleter.cs
@@ -0,0 +1,12 @@
+﻿using SquadManager.Dtos.Dogs;
+using SquadManager.Services.Interfaces.Dog;
+
+namespace SquadManager.Services.Core.Dogs;
+
+public class EfDogDeleter : IDogDeleter
+{
+    public async Task<DogDto> DeleteDog(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
