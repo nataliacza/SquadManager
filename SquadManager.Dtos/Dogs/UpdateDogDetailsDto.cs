d6d577c41f6c0d091b9aa203ef2c1bb20272da05
Natalia Czapska <czapskanatalia@gmail.com>
2022-07-05 10:53:08 +0200

    Update dto prepared

---
+++ SquadManager.Dtos/Dogs/UpdateDogDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SquadManager.Dtos.Dogs;
+
+public class UpdateDogDetailsDto
+{
+    public string? Breeder { get; set; }
+    public string? Gender { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? ChipNumber { get; set; }
+}
