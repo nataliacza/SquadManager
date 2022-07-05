ca9c8fdc2728233fa7d2501020c3b9b0faaf41be
Natalia Czapska <czapskanatalia@gmail.com>
2022-07-05 09:29:27 +0200

    Service draft prepared

---
+++ SquadManager.Services/Core/Dogs/EfDogUpdater.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using SquadManager.Database;
+using SquadManager.Dtos.Dogs;
+using SquadManager.Services.Interfaces.Dog;
+
+namespace SquadManager.Services.Core.Dogs;
+
+public class EfDogUpdater : IDogUpdater
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public EfDogUpdater(ApplicationDbContext dbContext, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<DogDto> UpdateDog(Guid id, SaveDogDto updateDto)
+    {
+        throw new NotImplementedException();
+    }
+}
