4ade0666a4bb9ffe8b975e09880ef6b5faab3eb8
Natalia Czapska <czapskanatalia@gmail.com>
2022-07-05 13:22:47 +0200

    Deleter service implemented

--- SquadManager.Services/Core/Dogs/EfDogDeleter.cs
+++ SquadManager.Services/Core/Dogs/EfDogDeleter.cs
@@ -1,12 +1,37 @@
-﻿using SquadManager.Dtos.Dogs;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SquadManager.Database;
+using SquadManager.Dtos.Dogs;
 using SquadManager.Services.Interfaces.Dog;
 
 namespace SquadManager.Services.Core.Dogs;
 
 public class EfDogDeleter : IDogDeleter
 {
+    private readonly ApplicationDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public EfDogDeleter(ApplicationDbContext dbContext, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+
     public async Task<DogDto> DeleteDog(Guid id)
     {
-        throw new NotImplementedException();
+        var dog = await _dbContext.Dogs
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (dog == null)
+        {
+            return null!;
+        }
+
+        _dbContext.Remove(dog);
+        await _dbContext.SaveChangesAsync();
+
+        var dto = _mapper.Map<DogDto>(dog);
+
+        return dto;
     }
 }
